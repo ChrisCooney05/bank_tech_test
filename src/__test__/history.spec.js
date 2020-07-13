c1741a61e7de688b3ce938d0dbe23d971dd686c6
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-13 15:47:28 +0100

    formated History.withdraw/deposit so funds are formated to two des places

--- src/__test__/history.spec.js
+++ src/__test__/history.spec.js
@@ -13,20 +13,30 @@ describe("History", () => {
 
   test("adds transaction to history when a deposit is made", () => {
     history.deposit("10/01/2020", 1000, 1000);
-    expect(history.transactions[0]).toEqual(["10/01/2020", 1000, "", 1000]);
+    expect(history.transactions[0]).toEqual([
+      "10/01/2020",
+      "1000.00",
+      "",
+      "1000.00",
+    ]);
   });
 
   test("Adds transaction to history when a withdrawal is made", () => {
     history.withdraw("10/02/2020", 500, 500);
-    expect(history.transactions[0]).toEqual(["10/02/2020", "", 500, 500]);
+    expect(history.transactions[0]).toEqual([
+      "10/02/2020",
+      "",
+      "500.00",
+      "500.00",
+    ]);
   });
 
   test("Returns full transaction history as an array of arrays", () => {
     history.deposit("10/01/2020", 1000, 1000);
     history.withdraw("10/02/2020", 250, 750);
     expect(history.returnTransactionHistory()).toEqual([
-      ["10/01/2020", 1000, "", 1000],
-      ["10/02/2020", "", 250, 750],
+      ["10/01/2020", "1000.00", "", "1000.00"],
+      ["10/02/2020", "", "250.00", "750.00"],
     ]);
   });
 });
--- src/history.js
+++ src/history.js
@@ -4,11 +4,15 @@ export default class History {
   }
 
   deposit(date, funds, balance) {
-    this.transactions.push([date, funds, "", balance]);
+    let fundsFloat = funds.toFixed(2);
+    let balanceFloat = balance.toFixed(2);
+    this.transactions.push([date, fundsFloat, "", balanceFloat]);
   }
 
   withdraw(date, funds, balance) {
-    this.transactions.push([date, "", funds, balance]);
+    let fundsFloat = funds.toFixed(2);
+    let balanceFloat = balance.toFixed(2);
+    this.transactions.push([date, "", fundsFloat, balanceFloat]);
   }
 
   returnTransactionHistory() {
