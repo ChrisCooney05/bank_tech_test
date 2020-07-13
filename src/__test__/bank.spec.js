b22544ed06abb98b6c83fe3bbf0c910d0d91d2f2
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-13 12:12:28 +0100

    added second passing test

--- src/__test__/bank.spec.js
+++ src/__test__/bank.spec.js
@@ -1,8 +1,18 @@
 import Bank from "../bank";
 
 describe("Bank", () => {
+  let bank;
+
+  beforeEach(() => {
+    bank = new Bank();
+  });
+
   test("bank can be constructed with a 0 balance", () => {
-    const bank = new Bank();
     expect(bank.balance).toBe(0);
+  });
+
+  test("user can deposit money into the account", () => {
+    bank.deposit(1000);
+    expect(bank.balance).toBe(1000);
   });
 });
--- src/bank.js
+++ src/bank.js
@@ -2,4 +2,8 @@ export default class Bank {
   constructor() {
     this.balance = 0;
   }
+
+  deposit(num) {
+    this.balance += num;
+  }
 }
