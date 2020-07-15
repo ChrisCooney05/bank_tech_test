454eeb9e1cd7687d6db5b1da0c41829b51f8cd3a
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-15 08:14:28 +0100

    final feature test added

--- src/__test__/features.spec.js
+++ src/__test__/features.spec.js
@@ -2,9 +2,19 @@ const Bank = require("../bank");
 
 describe("Bank", () => {
   let bank;
+  let date;
+
+  function getDate() {
+    let t = new Date();
+    let dd = t.getDate();
+    let mm = t.getMonth() + 1;
+    let yy = t.getFullYear();
+    return `${dd}/${mm}/${yy}`;
+  }
 
   beforeEach(() => {
     bank = new Bank();
+    date = getDate();
   });
 
   test("User can depost money in the bank", () => {
@@ -16,5 +26,15 @@ describe("Bank", () => {
     bank.deposit(1000);
     bank.withdraw(500);
     expect(bank.currentBalance()).toEqual(500);
+  });
+
+  test("User can request a statement and everything is logged correctly formatted and in rev chronological order", () => {
+    console.log = jest.fn();
+    bank.deposit(1000);
+    bank.withdraw(250);
+    bank.statement();
+    expect(console.log).toHaveBeenCalledWith(
+      `Date || Credit || Debit || Balance\n${date} ||  || 250.00 || 750.00\n${date} || 1000.00 ||  || 1000.00`
+    );
   });
 });
